b25aa237441315d33a34e9394058f852e78748c2
Bognár Bence <bbence210@gmail.com>
2023-05-18 20:22:48 +0200

    Signalr

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    entry: './src/Producer.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    }
+};
